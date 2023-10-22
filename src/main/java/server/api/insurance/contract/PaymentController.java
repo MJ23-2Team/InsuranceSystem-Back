27dccd27796a3ceeb9e1c5704e0cf911685a9cf7
Seongje Home <seongje00416@gmail.com>
2023-10-22 16:36:32 +0900

    controller 기능 추가

--- src/main/java/server/api/insurance/contract/PaymentController.java
+++ src/main/java/server/api/insurance/contract/PaymentController.java
@@ -1,10 +1,24 @@
 package server.api.insurance.contract;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import server.api.insurance.customer.CustomerDto;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 public class PaymentController {
     private final PaymentService paymentService;
+
+    @PostMapping("/payment")
+    public void add(@RequestBody PaymentDto request) {paymentService.add(request);}
+    @GetMapping("/payment")
+    public PaymentDto retrieve(@RequestParam int id) {return paymentService.retrieve(id);}
+    @GetMapping("/payment/getAll")
+    public List<PaymentDto> retrieveAll() {return paymentService.retrieveAll();}
+    @PutMapping("/payment")
+    public void update(@RequestBody PaymentDto request) {paymentService.update(request);}
+    @DeleteMapping("/payment")
+    public void delete(@RequestParam int id) {paymentService.delete(id);}
 }
