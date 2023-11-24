33f31c7dfefd4b55e687913d9910f4131cd5f5ce
Seongje Home <seongje00416@gmail.com>
2023-11-24 12:21:11 +0900

    2023-11-24 멘토링

--- src/main/java/server/app/insurance/intra/repository/EducationRepository.java
+++ src/main/java/server/app/insurance/intra/repository/EducationRepository.java
@@ -2,7 +2,10 @@ package server.app.insurance.intra.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import server.app.insurance.intra.dto.EducationDto;
 import server.app.insurance.intra.entity.Education;
+
+import java.util.List;
 
 @Repository
 public interface EducationRepository extends JpaRepository<Education,Integer> {
