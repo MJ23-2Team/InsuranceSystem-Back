f52ba61356ba7faee6d022df1e133c42441a8871
SEONGJE <seongje00416@gmail.com>
2023-11-29 13:09:33 +0900

    Add Repository Function

--- src/main/java/server/app/insurance/user/employee/repository/AdviceNoteRepository.java
+++ src/main/java/server/app/insurance/user/employee/repository/AdviceNoteRepository.java
@@ -1,9 +1,16 @@
 package server.app.insurance.user.employee.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import server.app.insurance.user.employee.entity.AdviceNote;
 
+import java.util.List;
+
 @Repository
 public interface AdviceNoteRepository extends JpaRepository<AdviceNote,Integer> {
+
+    @Query( "SELECT a FROM AdviceNote a WHERE a.customer.customerID=:customerId" )
+    List<AdviceNote> findByCustomerId( @Param("customerId") int customerId );
 }
