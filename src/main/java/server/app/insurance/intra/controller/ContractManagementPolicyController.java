243168b534fd98275b17ea7571cc3f1ed9bba1e3
wodnd0131 <wodnd0131@naver.com>
2023-12-05 13:17:55 +0900

    fix:merge

--- src/main/java/server/app/insurance/intra/controller/ContractManagementPolicyController.java
+++ src/main/java/server/app/insurance/intra/controller/ContractManagementPolicyController.java
@@ -10,39 +10,10 @@ import java.util.List;
 
 @Tag(name = "ContractManagementPolicy 컨트롤러", description = "ContractManagementPolicy API입니다.")
 @RestController
-<<<<<<< HEAD
-=======
 @RequestMapping( "/contractManagement")
->>>>>>> 92d8f69c5dcec6a63900125aacd631e7a0e7b0e9
 @RequiredArgsConstructor
 public class ContractManagementPolicyController {
     private final ContractManagementPolicyList contractManagementPolicyList;
-
-<<<<<<< HEAD
-    @PostMapping( "/contractmanagementpolicy")
-    public void add(@RequestBody ContractManagementPolicyDto request ){
-        contractManagementPolicyList.add( request );
-    }
-
-    @GetMapping( "/contractmanagementpolicy")
-    public ContractManagementPolicyDto retrieve( @RequestParam int id ){
-        return contractManagementPolicyList.retrieve( id );
-    }
-
-    @GetMapping( "/contractmanagementpolicy/getAll" )
-    public List<ContractManagementPolicyDto> retrieveAll(){
-        return contractManagementPolicyList.retrieveAll();
-    }
-
-    @PutMapping( "/contractmanagementpolicy" )
-    public void update( @RequestBody ContractManagementPolicyDto request ){
-        contractManagementPolicyList.update( request );
-    }
-
-    @DeleteMapping( "/contractmanagementpolicy" )
-    public void delete( @RequestParam int id ){
-        contractManagementPolicyList.delete( id );
-=======
     @PostMapping( "/add")
     public void createContractManagementPolicy(@RequestBody ContractManagementPolicyDto request ){
         contractManagementPolicyList.createContractManagementPolicy( request );
@@ -66,6 +37,5 @@ public class ContractManagementPolicyController {
     @DeleteMapping( "/delete" )
     public void deleteContractManagementPolicy( @RequestParam int id ){
         contractManagementPolicyList.deleteContractManagementPolicy( id );
->>>>>>> 92d8f69c5dcec6a63900125aacd631e7a0e7b0e9
     }
 }
