97ae4af9b5ef5d9a3a1ee8779f5d520f610aa34e
Seongje Home <seongje00416@gmail.com>
2023-12-05 22:39:27 +0900

    기능 보완 및 수정, 추가

--- src/main/java/server/app/insurance/intra/repository/EducationStudentRepository.java
+++ src/main/java/server/app/insurance/intra/repository/EducationStudentRepository.java
@@ -1,9 +1,16 @@
 package server.app.insurance.intra.repository;
 
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import server.app.insurance.intra.entity.EducationStudent;
 
+import java.util.List;
+
 @Repository
 public interface EducationStudentRepository extends JpaRepository<EducationStudent,Integer> {
+
+    @Query( "SELECT e FROM EducationStudent e WHERE e.education.educationID=:id")
+    List<EducationStudent> findByEducationId(@Param("id") int id );
 }
